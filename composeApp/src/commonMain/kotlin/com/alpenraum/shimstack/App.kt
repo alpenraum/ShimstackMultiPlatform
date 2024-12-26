4f0efc2dbbafd250c3aa80a2c867c2b8283face8
Finn Zimmer <finn.zimmer@bawaggroup.com>
2024-12-26 18:10:07 +0100

    added initialization step

--- composeApp/src/commonMain/kotlin/com/alpenraum/shimstack/App.kt
+++ composeApp/src/commonMain/kotlin/com/alpenraum/shimstack/App.kt
@@ -3,15 +3,19 @@ package com.alpenraum.shimstack
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.alpenraum.shimstack.base.di.ShimstackGeneratedModule
 import com.alpenraum.shimstack.base.di.databaseModule
 import com.alpenraum.shimstack.base.di.navigationModule
 import com.alpenraum.shimstack.base.di.platformModule
+import com.alpenraum.shimstack.data.datastore.ShimstackDatastore
+import com.alpenraum.shimstack.domain.InitializeAppUseCase
 import com.alpenraum.shimstack.ui.base.compose.theme.AppTheme
 import com.alpenraum.shimstack.ui.base.navigation.ShimstackNavHost
 import org.koin.compose.KoinApplication
+import org.koin.compose.koinInject
 import org.koin.ksp.generated.module
 
 @Composable
@@ -19,10 +23,25 @@ fun App() {
     KoinApplication(application = {
         modules(navigationModule(), ShimstackGeneratedModule().module, databaseModule(), platformModule())
     }) {
+        initializeApp()
+
         AppTheme {
             Surface(Modifier.fillMaxSize()) {
                 val navController = rememberNavController()
                 ShimstackNavHost(navController, modifier = Modifier)
+            }
+        }
+    }
+}
+
+@Composable
+private fun initializeApp() {
+    val datastore = koinInject<ShimstackDatastore>()
+    val initializeAppUseCase = koinInject<InitializeAppUseCase>()
+    LaunchedEffect(Unit) {
+        datastore.isOnboardingCompleted.collect {
+            if (!it) {
+                initializeAppUseCase()
             }
         }
     }
